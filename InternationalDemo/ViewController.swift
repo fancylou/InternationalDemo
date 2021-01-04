e5e01a542d3074786bd65c8928d979140aa8af66
fancy <fancylou@outlook.com>
2021-01-04 13:35:33 +0800

    Initial Commit

---
+++ InternationalDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternationalDemo
+//
+//  Created by FancyLou on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
