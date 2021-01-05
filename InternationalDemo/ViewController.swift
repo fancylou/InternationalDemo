87234ae5947f6f7dbf290070ca0f48de2bcf7d94
fancy <fancylou@outlook.com>
2021-01-05 10:20:01 +0800

    添加 BartyCrouch 和 Swiftgen 的支持

--- InternationalDemo/ViewController.swift
+++ InternationalDemo/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapChangeLabel(_ sender: UIButton) {
+        self.helloLabel.text = L10n.First.helloChange
+    }
+    
 }
 
